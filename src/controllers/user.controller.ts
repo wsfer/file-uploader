c4af1f050b8b367d3751a0725da4e257b4641654
wsfer <93054415+wsfer@users.noreply.github.com>
2025-08-01 18:26:11 -0300

    feat: add index and login routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,16 +2,16 @@ import bcrypt from 'bcryptjs';
 import asyncHandler from 'express-async-handler';
 import prisma from '../modules/prisma';
 
+const getIndex = asyncHandler(async (req, res) => {
+  res.render('user/index');
+});
+
 const getLogin = asyncHandler(async (req, res) => {
   res.render('user/login');
 });
 
 const getRegister = asyncHandler(async (req, res) => {
   res.render('user/register');
-});
-
-const postLogin = asyncHandler(async (req, res) => {
-  res.redirect('/');
 });
 
 const postRegister = asyncHandler(async (req, res) => {
@@ -25,4 +25,4 @@ const postRegister = asyncHandler(async (req, res) => {
   res.redirect('/');
 });
 
-export default { getLogin, getRegister, postLogin, postRegister };
+export default { getIndex, getLogin, getRegister, postRegister };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import userController from '../controllers/user.controller';
+import passport from '../modules/passport';
 
 const userRouter = Router();
 
+userRouter.get('/', userController.getIndex);
 userRouter.get('/login', userController.getLogin);
 userRouter.get('/register', userController.getRegister);
-userRouter.post('/login', userController.postLogin);
+userRouter.post('/login', passport.authenticate('local'));
 userRouter.post('/register', userController.postRegister);
 
 export default userRouter;
