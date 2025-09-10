2e5b91b0a5e1b74fa78e4580f4b00dbd14aaf3ef
wsfer <93054415+wsfer@users.noreply.github.com>
2025-09-10 15:49:09 -0300

    feat: add subfolder relation on database folder model

---
+++ prisma/migrations/20250910184757_subfolder/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Folder" ADD COLUMN     "parentId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
