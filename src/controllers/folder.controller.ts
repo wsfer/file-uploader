21b66b1fb9c1ba84ffa29c046119cdee14f5b889
wsfer <93054415+wsfer@users.noreply.github.com>
2025-09-15 16:32:20 -0300

    feat: add handlers to check for user authentication, user permission or folder not found on folder controller

--- src/controllers/folder.controller.ts
+++ src/controllers/folder.controller.ts
@@ -2,44 +2,76 @@ import asyncHandler from 'express-async-handler';
 import prisma from '../modules/prisma';
 import { User } from '../../generated/prisma';
 
-// TODO: create a middleware to check if user is logged in before to stop typescript errors
-const getUserRootFolder = asyncHandler(async (req, res) => {
-  const user: User = req.user;
-  const userRootFolder = await prisma.folder.findFirst({
-    where: { parentId: null, ownerId: user.id },
-    include: {
-      subfolders: true,
-    },
-  });
+// TODO: return a pretty HTML page for 404 and 403 responses
+// TODO: validate inputs
 
-  res.render('folder/index', { folder: userRootFolder });
+const getUserRootFolder = asyncHandler(async (req, res) => {
+  const isLoggedIn = Boolean(req.user);
+
+  if (isLoggedIn) {
+    const user: User = req.user as User;
+    const userRootFolder = await prisma.folder.findFirst({
+      where: { parentId: null, ownerId: user.id },
+      include: {
+        subfolders: true,
+      },
+    });
+
+    return res.render('folder/index', { folder: userRootFolder });
+  }
+
+  return res.status(401).redirect('/login');
 });
 
 const getFolder = asyncHandler(async (req, res) => {
-  const folderId: string = req.params.id;
-  const folder = await prisma.folder.findUnique({
-    where: { id: folderId },
-    include: {
-      parentFolder: true,
-      subfolders: true,
-    },
-  });
+  const isLoggedIn = Boolean(req.user);
 
-  res.render('folder/index', { folder: folder });
+  if (isLoggedIn) {
+    const user: User = req.user as User;
+    const folderId: string = req.params.id;
+    const folder = await prisma.folder.findUnique({
+      where: { id: folderId },
+      include: {
+        parentFolder: true,
+        subfolders: true,
+      },
+    });
+
+    if (!folder) {
+      return res.status(404).send('Not found');
+    }
+
+    // TODO: add share feature and check if another user can access
+    const isOwner = folder.ownerId === user.id;
+
+    if (isOwner) {
+      return res.render('folder/index', { folder: folder });
+    }
+
+    return res.status(403).send('Not allowed');
+  }
+
+  return res.status(401).redirect('/login');
 });
 
 const postFolder = asyncHandler(async (req, res) => {
-  const user: User = req.user;
-  const parentFolderId: string = req.params.id;
-  const newFolder = await prisma.folder.create({
-    data: {
-      name: req.body.name,
-      owner: { connect: { id: user.id } },
-      parentFolder: { connect: { id: parentFolderId } },
-    },
-  });
+  const isLoggedIn = Boolean(req.user);
 
-  res.redirect(`/folder/${newFolder.id}`);
+  if (isLoggedIn) {
+    const user: User = req.user as User;
+    const parentFolderId: string = req.params.id;
+    const newFolder = await prisma.folder.create({
+      data: {
+        name: req.body.name,
+        owner: { connect: { id: user.id } },
+        parentFolder: { connect: { id: parentFolderId } },
+      },
+    });
+
+    return res.redirect(`/folder/${newFolder.id}`);
+  }
+
+  return res.status(401).redirect('/login');
 });
 
 const renameFolder = asyncHandler(async (req, res) => {
