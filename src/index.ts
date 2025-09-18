96279f6bcbb71f1fee8afb21d19cf5a57661428b
wsfer <93054415+wsfer@users.noreply.github.com>
2025-09-18 16:02:42 -0300

    feat: rename folder route to drive route

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/', userRouter);
-app.use('/folder', folderRouter);
+app.use('/drive', folderRouter);
 app.use('/file', fileRouter);
 
 app.use(errorHandler);
