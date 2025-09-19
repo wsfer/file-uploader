999a8a81d99a0c0bc71ab22f84d50cf93eca481a
wsfer <93054415+wsfer@users.noreply.github.com>
2025-09-19 16:20:05 -0300

    feat: add file mimetype, path and size on file database model

---
+++ prisma/migrations/20250919191914_file_data/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `mimetype` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `path` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `size` to the `File` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "File" ADD COLUMN     "mimetype" TEXT NOT NULL,
+ADD COLUMN     "path" TEXT NOT NULL,
+ADD COLUMN     "size" INTEGER NOT NULL;
