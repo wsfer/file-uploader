ba3989e24d147ef7b34f9e454b4ae265908a3133
wsfer <93054415+wsfer@users.noreply.github.com>
2025-10-03 15:58:12 -0300

    feat: update error handler middleware to handle http errors

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,16 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 
+const defaultError: HTTPError = {
+  name: 'ServerError',
+  message: 'Internal server error',
+  statusCode: 500,
+};
+
 // TODO:
 // - render an error page
 // - create different custom error types
 function errorHandler(
-  err: Error,
+  err: Error | HTTPError,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
+  const isHTTPError = (err as HTTPError).statusCode !== undefined;
+
+  if (isHTTPError) {
+    return res.render('errors/index', { error: err });
+  }
+
   console.error(err);
-  res.status(500).end();
+  return res.render('errors/index', { error: defaultError });
 }
 
 export default errorHandler;
