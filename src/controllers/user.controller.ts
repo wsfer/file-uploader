65e6e9837b77c0936cd46141af83a623dcdae961
wsfer <93054415+wsfer@users.noreply.github.com>
2025-10-31 17:05:25 -0300

    feat: add error validation on register form and automatic login for newly created users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,9 @@
+import { validationResult } from 'express-validator';
 import bcrypt from 'bcryptjs';
 import asyncHandler from 'express-async-handler';
 import passport from '../modules/passport';
 import prisma from '../modules/prisma';
+import validateUser from '../middlewares/validateUser.middleware';
 
 const getIndex = asyncHandler(async (req, res) => {
   res.render('user/index');
@@ -20,22 +22,37 @@ const postLogin = passport.authenticate('local', {
   failureRedirect: '/login',
 });
 
-const postRegister = asyncHandler(async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const userRootFolder = { name: '.' };
+const postRegister = [
+  validateUser,
+  asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    const errors = validationResult(req);
 
-  await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-      folders: { create: [userRootFolder] },
-    },
-  });
+    if (!errors.isEmpty()) {
+      return res.render('user/register', {
+        errors: errors.mapped(),
+        values: req.body,
+      });
+    }
 
-  res.redirect('/');
-});
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userRootFolder = { name: '.' };
+    const createdUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+        folders: { create: [userRootFolder] },
+      },
+    });
+
+    // Automatically login the created user
+    req.login(createdUser, (err) => {
+      if (err) throw err;
+      res.redirect('/drive');
+    });
+  }),
+];
 
 const postLogout = asyncHandler(async (req, res, next) => {
   req.logout((err) => {
