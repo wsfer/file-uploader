17cfe408fabe1b8b90a0721a80b1af69c4601b55
wsfer <93054415+wsfer@users.noreply.github.com>
2025-11-08 11:24:50 -0300

    feat: add folder validation

--- src/controllers/folder.controller.ts
+++ src/controllers/folder.controller.ts
@@ -1,10 +1,11 @@
+import { validationResult } from 'express-validator';
 import { User } from '../../generated/prisma';
 import asyncHandler from 'express-async-handler';
 import prisma from '../modules/prisma';
 import NotFoundError from '../errors/NotFound.error';
+import validateFolder from '../middlewares/validateFolder.moddleware';
 
 // TODO: return a pretty HTML page for 404 and 403 responses
-// TODO: validate inputs
 
 const getUserRootFolder = asyncHandler(async (req, res) => {
   const isLoggedIn = Boolean(req.user);
@@ -57,25 +58,52 @@ const getFolder = asyncHandler(async (req, res) => {
   return res.status(401).redirect('/login');
 });
 
-const postFolder = asyncHandler(async (req, res) => {
-  const isLoggedIn = Boolean(req.user);
-
-  if (isLoggedIn) {
-    const user: User = req.user as User;
-    const parentFolderId: string = req.params.id;
-    const newFolder = await prisma.folder.create({
-      data: {
-        name: req.body.name,
-        owner: { connect: { id: user.id } },
-        parentFolder: { connect: { id: parentFolderId } },
+const postFolder = [
+  validateFolder,
+  asyncHandler(async (req, res) => {
+    const isLoggedIn = Boolean(req.user);
+    const errors = validationResult(req);
+    const parentFolder = await prisma.folder.findUnique({
+      where: { id: req.params.id },
+      include: {
+        parentFolder: true,
+        subfolders: true,
+        files: true,
       },
     });
 
-    return res.redirect(`/drive/${newFolder.id}`);
-  }
+    if (!parentFolder) {
+      throw new NotFoundError('Folder not found');
+    }
 
-  return res.status(401).redirect('/login');
-});
+    if (!errors.isEmpty() && isLoggedIn) {
+      const isRootFolder = parentFolder.parentFolder === null;
+      const viewToRender = isRootFolder ? 'folder/index' : 'folder/folder';
+
+      return res.render(viewToRender, {
+        errors: errors.mapped(),
+        values: req.body,
+        folder: parentFolder,
+      });
+    }
+
+    if (isLoggedIn) {
+      const user: User = req.user as User;
+      const parentFolderId: string = req.params.id;
+      const newFolder = await prisma.folder.create({
+        data: {
+          name: req.body.name,
+          owner: { connect: { id: user.id } },
+          parentFolder: { connect: { id: parentFolderId } },
+        },
+      });
+
+      return res.redirect(`/drive/${newFolder.id}`);
+    }
+
+    return res.status(401).redirect('/login');
+  }),
+];
 
 const renameFolder = asyncHandler(async (req, res) => {
   res.send('not implemented');
